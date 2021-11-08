ea5bb5bbd0e587399f482574d7b5bff8cc25e8f6
Jason <tkcjason@gmail.com>
2021-11-08 20:43:30 +0000

    Don't treat empty line as a word

--- src/main/java/com/example/synalogiktest/service/WordsService.java
+++ src/main/java/com/example/synalogiktest/service/WordsService.java
@@ -44,6 +44,9 @@ public class WordsService {
             bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
             // Read file line by line, as opposed to a large String, which may have performance issues if reading large file
             while ((line = bufferedReader.readLine()) != null) {
+                if (line.trim().length() == 0) {
+                    continue;
+                }
                 String[] wordsInLine = line.split(" ");
                 words.addAll(Arrays.asList(wordsInLine));
             }
